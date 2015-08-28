dc59fa095262119d5560422c370bc182c91e8072
eliran zach <thehobbit85@gmail.com>
2015-08-29 01:34:04 +0300

    bug fix

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = {
   app: require.resolve('./generators/app'),
   boilerplate: require.resolve('./generators/boilerplate'),
   editorconfig: require.resolve('./generators/editorconfig'),
-  eslint: require.resolve('./generators/eslint'),
   git: require.resolve('./generators/git'),
   gulp: require.resolve('./generators/gulp'),
   readme: require.resolve('./generators/readme'),
